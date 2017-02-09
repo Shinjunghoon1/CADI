a489e9d7dbe60c67849ad49b96eeb3ec8568a37a
shinjunghoon <sjh0826kr@naver.com>
2017-02-09 15:23:28 +0900

    no message

--- 1week/MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ 1week/MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package org.androidtown.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void Click1(View v){
-        Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                switch (v.getId()) {
+                    case R.id.btn:
+                        Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다", Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+
+
+            });
 
 
     }
-
 }
+
+
+
+
+
+
